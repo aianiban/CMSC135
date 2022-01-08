3fa76d1da188acf2cd9320cf489aec88df983d10
Adrianne Aniban <aianiban@up.edu.ph>
2022-01-08 22:01:07 +0800

    Update data.php

--- chat/php/data.php
+++ chat/php/data.php
@@ -5,26 +5,15 @@
                 OR incoming_msg_id = {$outgoing_id}) ORDER BY msg_id DESC LIMIT 1";
         $query2 = mysqli_query($conn, $sql2);
         $row2 = mysqli_fetch_assoc($query2);
-        if(mysqli_num_rows($query2) > 0){
-            $result = $row2['msg'];
-        } else{
-            $result = "No message available";
+        (mysqli_num_rows($query2) > 0) ? $result = $row2['msg'] : $result ="No message available";
+        (strlen($result) > 28) ? $msg =  substr($result, 0, 28) . '...' : $msg = $result;
+        if(isset($row2['outgoing_msg_id'])){
+            ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $you = "";
+        }else{
+            $you = "";
         }
-
-        //message preview
-        if(strlen($result) > 28){
-            $msg = substr($result, 0, 28) . "...";
-        } else {$msg = $result;}
-
-        if($outgoing_id == $row2['outgoing_msg_id']){
-            $you = "You: ";
-        } else {$you = "";}
-
-        //online status
-        if($row['status'] == "Offline now"){
-            $offline = "offline";
-        }  else {$offline = "";}
-
+        ($row['status'] == "Offline now") ? $offline = "offline" : $offline = "";
+        ($outgoing_id == $row['unique_id']) ? $hid_me = "hide" : $hid_me = "";
 
         $output .= '
         <a href="chat.php?user_id=' . $row['unique_id'] . '">
