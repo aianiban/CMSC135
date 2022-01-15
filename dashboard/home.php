defd5106d57e54ea1df739704ff7705c557ef890
Adrianne Aniban <aianiban@up.edu.ph>
2022-01-15 18:19:00 +0800

    Lipat modal.php + initial home page contents

--- dashboard/home.php
+++ dashboard/home.php
@@ -17,11 +17,12 @@
       $items .= '
         <a href="../profile/users-profile.php?user_id=' . $row2['unique_id'] .'">
           <div class="user-item">
-            <img src="" alt="" class="item-img">
+            <img src="../img/'. $row2['img'].'" alt="Admin" width="250" height="250">
             <p>' . $row2['fname'] . ' ' . $row2['lname'] . '</p>
+						<button type="button" class="btn btn-outline-light">Message</button>
+            <button type="button" class="btn btn-outline-light">Add Companion</button>
           </div>
         </a>';
-      
     }
               $request_count = 0;
               $sql3 = mysqli_query($conn, "SELECT * FROM companion_request WHERE user_two = {$_SESSION['unique_id']}");
@@ -29,7 +30,9 @@
                 $request_count = mysqli_num_rows($sql3);
               }
 ?>
-
+<?php
+						include_once "../dashboard/modal.php";
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -42,77 +45,6 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <script src="../javascript/users.js"></script>
-  <!-- Modal -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  <!-- Modal -->
-  <style>
-    .userlist {
-      position: relative;
-      top: 10px;
-      width: 100%;
-      height: 50px;
-      /* justify-content: center; */
-    }
-
-    .badge {
-      position: relative;
-      top: -10px;
-      right: 20px;
-      padding: 5px 5px;
-      border-radius: 50%;
-      background-color: red;
-      color: white;
-
-    }
-
-    .user-item{
-      background: linear-gradient(90deg, rgba(35,182,118,1) 0%, rgba(32,178,170,1) 100%);
-      width: 25%;
-      margin: auto;
-      color: white;
-      border-radius: 5px;
-      border: 1px solid black;
-    }
-
-    .companion-request{
-      overflow: auto;
-      /* width: ; */
-    }
-
-    .request-img{
-      display: inline;
-      width: 50px;
-      height: 50px;
-      right: 0%;
-      border-radius: 50%;
-    }
-
-    .img-name{
-      float: left;
-    }
-
-    .confirm-decline{
-      float: right;
-    }
-
-    .companion-request .img-name p,
-    .companion-request .confirm-decline i{
-      display: inline-block;
-    }
-
-    .modal-body ul{
-      list-style-type: none;
-    }
-
-    .compaion-request .confirm-decline i{
-      width: 50px;
-      height: 50px;
-    }
-
-  </style>
 
  </head>
 <body>
@@ -168,75 +100,17 @@
   
   </div>
     
-  <div class="dummy-text">
+  <<div class="dummy-text">
     <h2>Welcome to Accompany <?php echo $row['fname'];?>!</h2>
-    <h2>Find a companion now</h2>
+    <h4>Find a companion now</h4>
+    <div class="card">
+    <p>Accompany is a social media platform for BS Computer Science students and alumni of University of the Philippines Baguio. It is an online community whose main goal is to help struggling students in their computer science courses through connecting them to people who can give a helping hand. Users can find, add, and converse with other people whether you’re a freshman looking for a quick peer to peer tutorial, or a senior who wants to provide a more relatable perspective on computer science concepts. In this platform, users are called “companions”. Find your companion today!</p>
+    </div>
     <div class="userlist">
-      <!-- <a href="">
-        <div class="user-item">
-          <img src="" alt="" class="item-img">
-          <p>Sample User</p>
-        </div>
-      </a> -->
       <?php echo $items;?>
       
     </div>
   </div>
-
-
-
-<!-- Modal -->
-<div class="modal" tabindex="-1" role="dialog" id="request-modal">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Companion Requests</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <?php
-          if(mysqli_num_rows($sql3) > 0) {
-            while($row3 = mysqli_fetch_assoc($sql3)) {
-              $sql4 = mysqli_query($conn, "SELECT * FROM users WHERE unique_id = {$row3['user_one']}");
-              $user_requesting = mysqli_fetch_assoc($sql4);
-              $name = $user_requesting['fname'] . " " . $user_requesting['lname'];
-              $img = $user_requesting['img'];
-              echo '<ul>
-              <li>
-                <div class="companion-request">
-                  <div class="img-name">
-                    <img src="../img/' . $img . '" class="request-img">
-                    <a href="../profile/users-profile.php?user_id=' . $user_requesting['unique_id'] . '"><p>' . $name . '</p></a>
-                  </div>
-                  <div class="confirm-decline">                    
-                    <i class="fas fa-check-circle"></i>
-                  </div>
-                  
-                  
-                </div>    
-              </li>
-            </ul>';
-            }
-
-
-          } else {
-            echo 'No Companion Requests';
-          }
-        
-        
-        
-        
-        ?>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id="remove-request-confirm" data-dismiss="modal">Confirm</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
 </body>
 </html>
 
