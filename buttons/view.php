ffe9d1bba54245ad9683fcc25b45eabbc4a8bb91
Adrianne <aianiban@up.edu.ph>
2022-01-18 20:13:27 +0800

    Profile initial

--- buttons/view.php
+++ buttons/view.php
@@ -1,21 +1,21 @@
 <?php
-	include 'config.php';
+	include '../config.php';
 	$sql = "SELECT * FROM users WHERE unique_id = {$_SESSION['unique_id']}";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
 		while($row = $result->fetch_assoc()) {
 ?>	
 		<tr>
-			<td><?=$row['name'];?></td>
+			<td><?=$row['fname'];?></td>
 			<td><?=$row['email'];?></td>
-			<td><?=$row['phone'];?></td>
-			<td><?=$row['city'];?></td>
-			<td><button type="button" class="btn btn-success btn-sm update" data-toggle="modal" data-keyboard="false" data-backdrop="static" data-target="#update_country"
-			data-id="<?=$row['id'];?>"
-			data-name="<?=$row['name'];?>"
+			<td><?=$row['position'];?></td>
+			<td><?=$row['bio'];?></td>
+			<td><button type="button" class="btn btn-success btn-sm update" data-toggle="modal" data-keyboard="false" data-backdrop="static" data-target="#update_profile"
+			data-id="<?=$row['fname'];?>"
+			data-name="<?=$row['lname'];?>"
 			data-email="<?=$row['email'];?>"
-			data-phone="<?=$row['phone'];?>"
-			data-city="<?=$row['city'];?>"
+			data-phone="<?=$row['bio'];?>"
+			data-city="<?=$row['position'];?>"
 			">Edit</button></td>
 		</tr>
 <?php	
--- profile/profile.php
+++ profile/profile.php
@@ -170,7 +170,7 @@
 							<div class="row">
 								<div class="col-sm-3"></div>
 								<div class="col-sm-9 text-secondary">
-									<input type="button" class="btn btn-success px-4" id="edit_data" value="Edit Profile">
+									<button type="button" class="btn btn-success px-4 update" data-toggle="modal" data-target="#update_profile">Edit Profile</button></td>
 								</div>
 							</div>
 						</div>
@@ -208,5 +208,118 @@
 			</div>
 		</div>
 	</div>
+
+	<!-- Modal Update-->
+    <div class="modal fade" id="update_profile" role="dialog">
+		<div class="modal-dialog modal-sm">
+		  <div class="modal-content">
+			<div class="modal-header" style="color:#fff;background-color: #e35f14;padding:6px;">
+			  <h5 class="modal-title"><i class="fa fa-edit"></i> Update</h5>
+			 
+			</div>
+			<div class="modal-body">
+			
+				<!--1-->
+				<div class="row">
+					<div class="col-md-3">
+					    <label>Name</label>
+					</div>
+					<div class="col-md-9">
+						<input type="text" name="name_modal" id="name_modal" class="form-control-sm" required>
+					</div>	
+				</div>
+			    <!--2-->
+				<div class="row">
+					<div class="col-md-3">
+					    <label>Email</label>
+					</div>
+					<div class="col-md-9">
+						<input type="text" name="email_modal" id="email_modal" class="form-control-sm" required>
+					</div>	
+				</div>
+			    <!--3-->
+				<div class="row">
+					<div class="col-md-3">
+					    <label>Phone</label>
+					</div>
+					<div class="col-md-9">
+						<input type="text" name="phone_modal" id="phone_modal" class="form-control-sm" required>
+					</div>	
+				</div>
+				<!--4-->
+				<div class="row">
+					<div class="col-md-3">
+					    <label>City</label>
+					</div>
+					<div class="col-md-9">
+						<input type="text" name="city_modal" id="city_modal" class="form-control-sm" required>
+					</div>	
+				</div>
+				<input type="hidden" name="id_modal" id="id_modal" class="form-control-sm">
+			</div>
+			<div class="modal-footer" style="padding-bottom:0px !important; text-align:center !important;">
+			<p style="text-align:center;float:center;"><button type="submit" id="update_data" class="btn btn-default btn-sm" style="background-color: #e35f14;color:#fff;">Save</button>
+			<button type="button" class="btn btn-default btn-sm" data-dismiss="modal" style="background-color: #e35f14;color:#fff;">Close</button></p>
+			
+		  </div>
+		  </div>
+		</div>
+	</div>
+<!-- Modal End-->
+
+<script>/*
+$(document).ready(function() {
+	$.ajax({
+		url: "../buttons/view.php",
+		type: "POST",
+		cache: false,
+		success: function(dataResult){
+			alert("Successful!");
+			$('#table').html(dataResult);
+		}
+	});
+	$(function () {
+		$('#update_profile').on('show.bs.modal', function (event) {
+			var button = $(event.relatedTarget);
+			alert("Successful!");
+			/*Button that triggered the modal*/
+			/*var id = button.data('id');
+			var name = button.data('name');
+			var email = button.data('email');
+			var phone = button.data('phone');
+			var city = button.data('city');
+			var modal = $(this);
+			modal.find('#name_modal').val(name);
+			modal.find('#email_modal').val(email);
+			modal.find('#phone_modal').val(phone);
+			modal.find('#city_modal').val(city);
+			modal.find('#id_modal').val(id);
+		});
+    });
+	$(document).on("click", "#update_data", function() { 
+		$.ajax({
+			url: "edit.php",
+			type: "POST",
+			cache: false,
+			data:{
+				id: $('#id_modal').val(),
+				name: $('#name_modal').val(),
+				email: $('#email_modal').val(),
+				phone: $('#phone_modal').val(),
+				city: $('#city_modal').val(),
+			},
+			success: function(dataResult){
+				var dataResult = JSON.parse(dataResult);
+				if(dataResult.statusCode==200){
+					$('#update_profile').modal().hide();
+					alert('Data updated successfully !');
+					location.reload();					
+				}
+			}
+		});
+	}); 
+});*/
+</script>
+
 </body>
 </html>
